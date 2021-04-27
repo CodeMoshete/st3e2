4e9ce1bb83d5cb5b9a841c1c6934d4b583e4f19a
CodeMoshete <codemoshete@gmail.com>
2021-04-27 00:52:38 -0700

    Add generation script for billboards

---
+++ ST3E2-Unity/Assets/Source/Utils/BillboardBaker2.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class BillboardBaker2 : MonoBehaviour
+{
+    private const float TIMER_TIME = 0.5f;
+
+    public Camera ScreenshotCamera;
+    public GameObject TargetObject;
+    public float Distance;
+
+    private Transform cameraTransform;
+    private float horizontalAngle = 0f;
+    private float verticalAngle = -90f;
+    private float timer = TIMER_TIME;
+    private int rotationCounter = 0;
+
+    private void Start()
+    {
+        cameraTransform = ScreenshotCamera.transform;
+    }
+
+    private void Update()
+    {
+        if (timer <= 0f)
+        {
+            horizontalAngle += 45f;
+            timer = TIMER_TIME;
+            rotationCounter++;
+            if (rotationCounter >= 5)
+            {
+                horizontalAngle = 0f;
+                rotationCounter = 0;
+                verticalAngle += 45f;
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+
+        float yPos = Mathf.Sin(verticalAngle * Mathf.Deg2Rad) * Distance;
+        float hRadius = Mathf.Cos(verticalAngle * Mathf.Deg2Rad) * Distance;
+        float xPos = Mathf.Sin(horizontalAngle * Mathf.Deg2Rad) * hRadius;
+        float zPos = Mathf.Cos(horizontalAngle * Mathf.Deg2Rad) * hRadius;
+        cameraTransform.localPosition = new Vector3(xPos, yPos, zPos);
+        cameraTransform.LookAt(TargetObject.transform);
+
+        int diff = Mathf.RoundToInt(Mathf.Abs(Mathf.Abs(verticalAngle - 90f)));
+        Debug.Log("Angle: " + diff + ", " + (diff == 0));
+        if (diff == 0 || diff == 180)
+        {
+            Vector3 currentAngles = cameraTransform.localEulerAngles;
+            currentAngles.z = horizontalAngle;
+            cameraTransform.localEulerAngles = currentAngles;
+        }
+    }
+}
